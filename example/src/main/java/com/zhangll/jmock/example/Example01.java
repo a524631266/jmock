a1a77014d689bb08257177cf80cbd278a811f8bd
Zhang Liangliang <zhangll@homewell.com.cn>
2020-11-13 16:39:52 +0800

    添加案例1

---
+++ example/src/main/java/com/zhangll/jmock/example/Example01.java
@@ -0,0 +1,12 @@
+package com.zhangll.jmock.example;
+
+import com.zhangll.jmock.core.AnnotationMockContext;
+import com.zhangll.jmock.core.MockContext;
+
+public class Example01 {
+    public static void main(String[] args) {
+        AnnotationMockContext annotationMockContext = new AnnotationMockContext();
+
+//        annotationMockContext.mock()
+    }
+}
