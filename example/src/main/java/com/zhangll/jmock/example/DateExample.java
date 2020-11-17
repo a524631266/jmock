fe42e589a21b2a1e263fc58b2775e38ab5f46f55
Zhang Liangliang <zhangll@homewell.com.cn>
2020-11-17 14:31:47 +0800

    添加date案例

--- example/src/main/java/com/zhangll/jmock/example/DateExample.java
+++ example/src/main/java/com/zhangll/jmock/example/DateExample.java
@@ -10,28 +10,28 @@ import java.sql.Timestamp;
 
 @ToString
 class DatePojo{
-//    // 表示最小2010-10-10日,最大2010-10-20
-//    @BasicTokenInfo(min = "2010-10-10" , max = "2010-10-20")
-//    Date date;
-//    // 同上
-//    @BasicTokenInfo(min = "2010-10-10 00:10:20" , max = "2010-10-20 00:10:20")
-//    Timestamp timestamp;
-//
-//    // 同上
+    // 表示最小2010-10-10日,最大2010-10-20
+    @BasicTokenInfo(min = "2010-10-10" , max = "2010-10-20")
+    Date date;
+    // 同上
+    @BasicTokenInfo(min = "2010-10-10 00:10:20" , max = "2010-10-20 00:10:20")
+    Timestamp timestamp;
+
+    // 同上
     @BasicTokenInfo(min = "00:10:20" , max = "00:11:20")
     Time time;
-//
-//    // 步长为 2天
-//    @BasicTokenInfo(min = "2010-10-10" , max = "2010-10-20",step = "2")
-//    Date date2;
-//
-//    // 步长为2秒
-//    @BasicTokenInfo(min = "2010-10-10 00:10:20" , max = "2010-10-20 00:10:20", step = "2")
-//    Timestamp timestamp2;
-//
-//    // 步长为2秒
-//    @BasicTokenInfo(min = "00:10:20" , max = "00:11:20", step = "2")
-//    Time time2;
+
+    // 步长为 2天
+    @BasicTokenInfo(min = "2010-10-10" , max = "2010-10-20",step = "2")
+    Date date2;
+
+    // 步长为2秒
+    @BasicTokenInfo(min = "2010-10-10 00:10:20" , max = "2010-10-20 00:10:20", step = "2")
+    Timestamp timestamp2;
+
+    // 步长为2秒
+    @BasicTokenInfo(min = "00:10:20" , max = "00:11:20", step = "2")
+    Time time2;
 
 }
 
